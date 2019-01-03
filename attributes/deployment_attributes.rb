3d8c0210e7c8277a1aea2e4c3f9c560aaad94b44
Mobolaji Aina <mobolaji.aina@interswitchgroup.com>
2019-01-03 14:09:24 +0100

    change start command

--- attributes/deployment_attributes.rb
+++ attributes/deployment_attributes.rb
@@ -18,26 +18,32 @@ if node['platform'] == 'windows'
  default[:start_command]=' start cmd /k C:\Progra~1\MongoDB\Server\3.4\bin\mongod.exe --dbpath ..\..\..\..\..\MongoDB\datafiles\ --logpath=..\..\..\..\..\MongoDB\logs\mongolog.log  -v --logRotate rename  --logappend -f  ..\..\..\..\..\MongoDB\mongod.conf && cd ' + default[:local_repo_expansion_folder] + ' && start cmd /k npm start && taskkill /im ruby.exe /f'
  default[:start_app_only]=false
  default[:force_deploy]=false
+ default[:should_use_proxy]=false
+ default[:proxy_url]="http://172.16.10.239:8080"
 end 
  
  if node['platform'] != 'windows' 
  default[:repo_url]= "https://codeload.github.com/neoandrey/nodejs-kayxlavcomms/zip/master"
  default[:repo_user]= "neoandrey@yahoo.com"
- default[:local_repo_archive]=  "/tmp/nodejs-kayxlavcomms-master.zip"
- default[:local_repo_expansion_folder]=  "/tmp/nodejs/nodejs-kayxlavcomms-master/"
- default[:repo_expansion_folder]="/tmp/nodejs/"
+ default[:repo_name]="nodejs-kayxlavcomms-master"
+ default[:local_repo_archive]=  "/tmp/#{default[:repo_name]}.zip"
+ default[:local_repo_expansion_folder]=  "/tmp/nodejs/"
+ default[:repo_expansion_folder]="/opt/nodejs/"
  default[:site_folder]= "/opt/nodejs/kayxlavcomms/"
  default[:config_file_location]= "/opt/nodejs/kayxlavcomms/config/settings.json"
- default[:server_host_name]= "127.0.0.1"
+ default[:server_host_name]= "0.0.0.0"
  default[:server_port_number]= 9000
- default[:reload_port]= 9001
- default[:site_title]= "Backup and Restore Management Platform"
- default[:site_name]=  "Bakup and Restore Toolkit"
+ default[:reload_port]=9001
+ default[:site_title]= "KayXLavcomms"
+ default[:site_name]=  "KayXLavcomms Website"
  default[:mongo_host_name] = "mongo"
- default[:mongo_port_number]= "27017"
+ default[:mongo_port_number]= 27017
  default[:refresh_structure]=true
- default[:mongo_collection_name]="kayxlavcoms"
- default[:start_command]="cd /opt/nodejs/kayxlavcomms/ && npm start"
+ default[:mongo_collection_name]="kayxlavcomms"
+ default[:start_command]="cd #{default[:site_folder]} \n rm -rf package-lock.json   \n npm install \n  pgrep  #{default[:app_process_name]} | sudo xargs -n1 kill  \n bash start_kayxlav.sh"
  default[:start_app_only]=false
- default[:force_deploy]=false
+ default[:force_deploy]=true
+ default[:should_use_proxy]=false
+ default[:proxy_url]="http://172.16.10.239:8080"
+ default[:app_process_name]="node"
  end
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,10 +10,35 @@ local_file_time = Time.now
 source_file_time= Time.now
 should_deploy =  node[:force_deploy] 
 is_node_running = false
+should_use_proxy = node[:should_use_proxy]
+proxy_url   = nil
+if  should_use_proxy 
+	proxy_url   = node[:proxy_url]
+end
 
-
-if node['platform'] != 'windows'
+if node['platform']  == 'windows' &&  should_use_proxy
+	
+	execute 'set_proxy' do
+		command  "SETX HTTP_PROXY #{proxy_url}  &&  SETX HTTPS_PROXY #{proxy_url} &&  SETX FTP_PROXY #{proxy_url}"
+	end
+elseif node['platform'] != 'windows'
    include_recipe 'tar::default'
+   template "/etc/environment" do
+	source 'etc/environment.erb'
+	variables( 
+			  "proxy_url" => "#{proxy_url}"
+		  )
+  end
+  
+   template "root/.bashrc" do
+	source 'home/bashrc.erb'
+	variables( 
+			  "proxy_url" => "#{proxy_url}"
+			)
+  end
+  execute 'set_proxy' do
+		  command  " export http_proxy=#{proxy_url} &&  export https_proxy=#{proxy_url} &&  export ftp_proxy=#{proxy_url} &&  export no_proxy=127.0.0.1,localhost,0.0.0.0"
+  end
 end
 
 ruby_block 'get_app_repo_date' do
@@ -23,6 +48,7 @@ ruby_block 'get_app_repo_date' do
 	end
  end
 end 
+
 if node[:start_app_only]
 
 	if  node['platform'] == 'windows' 
@@ -50,11 +76,29 @@ if node[:start_app_only]
 					Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
 					command = 'ps -aux | grep node'
 					command_out = shell_out(command)
-					is_node_running =    command_out.stdout.nil? ||  command_out.stdout.empty?
+					is_node_running =  command_out.stdout.nil? ||  command_out.stdout.empty?
 			end	
 			  action:run
 		end
+	
+	
 		if  !is_node_running
+		is_node_installed = false
+		ruby_block "check_if_node_is_installed" do
+			block do
+					Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+					command = 'whereis node'
+					command_out = shell_out(command)
+					is_node_installed =    command_out.stdout.nil? ||  command_out.stdout.empty? || command_out.stdout=="node:" 
+			end	 
+			  action:run
+			  notifies :run, "execute[install_node]", :immediately
+		end
+		execute 'install_node' do
+				command "sudo yum remove nodejs.x86_64 -y && curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - && sudo yum install nodejs -y && cd #{node[:site_folder]} && npm install -g nodemon"
+				action :nothing
+			end
+	
 	#	execute 'start_node_app' do
 	#		command "#{node[:start_command]}"
 	#	  end
@@ -87,22 +131,33 @@ else
 		block do
 			source_file_time =	  File.stat("#{node[:local_repo_archive]}").mtime 
 			config_file = node[:config_file_location]
-			if   !File.exists?(config_file) || (source_file_time.to_i  > local_file_time.to_i) || node[:force_deploy] 
+			if   !File.exists?(config_file) || (source_file_time.to_i  > local_file_time.to_i) 
 				should_deploy = true 
 			end
 		end
 	end
 
-	puts "should_deploy #{should_deploy}"
 	if   should_deploy 
 		if node['platform'] == 'windows' 
 			execute "extract repo #{node[:local_repo_archive]}" do
-			  command  "PowerShell Expand-Archive -Path \" #{node[:local_repo_archive]} \" -DestinationPath \" #{node[:repo_expansion_folder]} \"  -Force"
+				command  "PowerShell Expand-Archive -Path \" #{node[:local_repo_archive]} \" -DestinationPath \" #{node[:repo_expansion_folder]} \"  -Force"
+				not_if { !should_deploy}
 			end
 		else
 
+		execute "install_unzip" do
+			command  "sudo yum install -y unzip"
+			not_if { !should_deploy}
+		end
+		directory "#{node[:repo_expansion_folder]}" do
+			owner 'neo'
+			group 'root'
+			mode '0755'
+			action :create
+		  end
 		 execute "extract repo: #{node[:local_repo_archive]}" do
-			  command  "unzip \" #{node[:local_repo_archive]} \" -d \" #{node[:repo_expansion_folder]} \" "
+				command  "sudo unzip -o \"#{node[:local_repo_archive]}\" -d \"#{node[:local_repo_expansion_folder]}\" "
+				not_if { !should_deploy}
 		end
 			  
 	
@@ -110,22 +165,29 @@ else
 
 					if node['platform'] == 'windows'
 						execute 'copy_files_from_extracted_folder_nodejs-kayxlavcomms-master-overwrite' do
-						  command  "xcopy /s /d /e \"#{node[:local_repo_expansion_folder]}*.*\"  \"#{node[:site_folder]}\" "
+							command  "xcopy /s /d /e \"#{node[:local_repo_expansion_folder]}*.*\"  \"#{node[:site_folder]}\" "
+							not_if { !should_deploy}
 						end
-					else
-					 node['platform'] != 'windows'
+					end
+					if node['platform'] != 'windows'
 					 execute 'copy_files_from_extracted_folder_nodejs_nodejs-kayxlavcomms-master' do
-						  command  "cp -rp \"#{node[:local_repo_expansion_folder]}*.*\"  \"#{node[:site_folder]}\""
-						end
-						  
+							command  "cp -r -p -u \"#{node[:local_repo_expansion_folder]}#{node[:repo_name]}\"/* \"#{node[:site_folder]}\""
+							not_if { !should_deploy}
+						end	  
 					end
 		else
-			ruby_block "rename_extracted_repo_folder" do
-				block do
-				::File.rename("#{node[:local_repo_expansion_folder]}","node[:local_repo_expansion_folder]")
-			end
+
+			directory "#{node[:site_folder]}" do
+			owner 'neo'
+			group 'root'
+			mode  '0766'
+			action :create
+		  end
+			execute "rename_extracted_repo_folder" do
+				command  "mv  \"#{node[:local_repo_expansion_folder]}#{node[:repo_name]}\"/* \"#{node[:site_folder]}\""
+			
 		end
-	
+		end
 		template "#{node[:config_file_location]}" do
 		  source 'config/settings.json.erb'
 		  variables( 
@@ -134,17 +196,15 @@ else
 					"reload_port" => node[:reload_port],
 					"site_title" => node[:site_title],
 					"site_name" =>  node[:site_name] ,
-					"mongo_host_name"  => node[:mongo_host_name] ,
+					"mongo_host_name"  => "#{node[:mongo_host_name]}" ,
 					"mongo_port_number" =>  node[:mongo_port_number] ,
 					"refresh_structure" => node[:refresh_structure],
-					"mongo_collection_name" => node[:mongo_collection_name],
+					"mongo_collection_name" =>  "#{node[:mongo_collection_name]}"
 				)
 		end
-    	end
-  end
+    end
 end
 end
-
 
 if  node['platform'] == 'windows' 
 	ruby_block "check_if_node_is_running" do
@@ -166,21 +226,43 @@ if  node['platform'] == 'windows'
 
 	end
  else 
-	ruby_block "check_if_node_is_running" do
+	ruby_block "check_if_#{node[:app_process_name]}_is_running" do
 		block do
 				Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
-				command = 'ps -aux | grep node'
+				command = "pgrep #{node[:app_process_name]}"
 				command_out = shell_out(command)
-				is_node_running =    command_out.stdout.nil? ||  command_out.stdout.empty?
+				is_node_running =  command_out.stdout.nil? ||  command_out.stdout.empty?
+				puts "is_node_running: #{is_node_running}"
 		end	
 		  action:run
 	end
+
+
 	if  !is_node_running
-#	execute 'start_node_app' do
-#		command "#{node[:start_command]}"
-#	  end
-      script 'start_node_app' do
-		code  "#{node[:start_command]}"
+	is_node_installed = false
+	execute 'clean_temp_folder' do
+		command "sudo rm -rf #{node[:local_repo_expansion_folder]}/*"
+	  end
+	  
+	ruby_block "check_if_node_is_installed" do
+		block do
+				Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+				command = 'whereis node'
+				command_out = shell_out(command)
+				is_node_installed =    command_out.stdout.nil? ||  command_out.stdout.empty? || command_out.stdout.to_s.tr(' ', '')=="node:" 
+		end	 
+		  action:run
+		  notifies :run, "execute[install_node]", :immediately
+	end
+	execute 'install_node' do
+			command "sudo yum remove nodejs.x86_64 -y && curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - && sudo yum install nodejs -y && cd #{node[:site_folder]} && npm install -g nodemon"
+	        action :nothing
+		end
+	execute  'set_access_permissions' do
+     command  "sudo find  #{node[:site_folder]} -type d -exec chmod 755 {} \\; &&  find  #{node[:site_folder]} -type f -exec chmod 757 {} \\; && chmod 757 #{node[:site_folder]}/node_modules"
+	end
+      execute 'start_node_app' do
+		command  "#{node[:start_command]}"
 	  end
 
 	end
